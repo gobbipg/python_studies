2c33a98ad6dd3de92861d5b0247a11b7e88732cd
gobbipg <paulogj.gobbip@gmail.com>
2025-01-18 22:04:40 -0300

    Explicando métodos / funções básicos em listas

--- aula48_part3.py
+++ aula48_part3.py
@@ -17,3 +17,11 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 
 #       0   1   2   3
 lista = [10, 20, 30, 40]
+lista.append('Gobbi')  # Adicionei a string Gobbi ao final da minha lista numerica
+nome = lista.pop()  # Aqui exclui o ultimo indice da minha lista (que era a string gobbi) / posso escolher qqlr indice para excluir, basta declarar no () no pop
+print(lista, nome)
+del lista[-1]  # Aqui eu deletei um indice da lista (caso não saiba qual o ultimo indice da lista, basta colocar o -1)
+# lista.clear()  # Aqui eu limpei toda a lista
+print(lista, nome)
+lista.insert(2, '100')  # Aqui eu inseri novos dados e movi os indices, basta colocar dois argumentos nos (), o primeiro é o indice que vc irá mexer e depois é o dado novo que vc irá acrescentar
+print(lista)
---
+++ aula48_part5.py
@@ -0,0 +1,5 @@
+"""
+Cuidados com dados mutáveis
+= - copiado o valor (imtáveis)
+= - aponta para o mesmo valor na memória (mutável)
+"""
